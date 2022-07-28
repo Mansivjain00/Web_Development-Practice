2c240a750ea76b2fc580c99890756dedfcef4474
Mansi V Jain <mansi.cs20@bmsce.ac.in>
2022-07-28 09:06:46 +0530

    commit

--- JavaSript/Ajax.js
+++ JavaSript/Ajax.js
@@ -11,9 +11,20 @@ function buttonClickHandler() {
     const xhr = new XMLHttpRequest();
 
     // Open the object
+    // GET REQUEST
     // true: for asynchronous request
     // What kind of request her? GET(for fetching) request ; POST(send data too,respond according to the data sent)
-    xhr.open('GET','mansi.txt',true);
+    // xhr.open('GET','https://jsonplaceholder.typicode.com/todos/1',true);
+
+    // you can say mansi.txt inplace of the url to get data from mansi.txt
+
+    // Post Request
+    // URL taken from dummy json rest api
+    
+    xhr.open('POST',`https://dummy.restapiexample.com/api/v1/create`,true);
+    // For post request you have to send some data
+    xhr.setRequestHeader('Content-type','application/x-www-form-urlencoded');
+
 
     // What to do on progress(Optional)
     xhr.onprogress = function () {
@@ -44,11 +55,57 @@ function buttonClickHandler() {
         // 3: Loading(Loading data)
         // 4: Done
         console.log("Ready state is: "+xhr.readyState);
-    }
+    } 
 
     // Send request
     // The text gets loaded without reloading the page
-    xhr.send();
+    // xhr.send();
+
+    // for Post
+    let params = "name&test&salary&123&age=23";
+    xhr.send(params);
+
+
+    console.log("We are done");
+    // This gets printed first and then , since we've set async to true, therefore the code runs asynchronously(xhr.open(...,...,true))
+    // if you set it to false, "we are done willbe printed later"
 
 }
 
+// Post request urn and params content ttaken from dummy json rest api
+
+let pop_btn = document.getElementById('Populate');
+
+pop_btn.addEventListener('click',popHandler);
+
+function popHandler() {
+    console.log("Button clicked: populate btn")
+    // Instantiate an XHR object
+    const xhr = new XMLHttpRequest();
+
+    // Open the object
+    xhr.open('GET','https://dummy.restapiexample.com/api/v1/employees',true);
+
+    xhr.onload = function () {
+        // 200 -->success
+        // 404 -->not found
+        if(this.status === 200){
+            let obj=JSON.parse(this.responseText);
+            console.log(obj);
+            let list = document.getElementById('list');
+            str="";
+            for(key in obj){
+                str =str + `<li>${obj[key].employee_name}</li>`;
+            }
+            list.innerHTML=str;
+        }
+        else{
+            console.error("Error occured");
+        }
+    }
+    
+    
+    xhr.send();
+
+    console.log("We are done frtching employees");
+}
