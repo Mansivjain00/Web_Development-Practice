494ff75476c3c1779a8c3de5ef6df91858273040
Mansi V Jain <mansi.cs20@bmsce.ac.in>
2022-08-02 00:00:25 +0530

    commit

---
+++ JavaSript/ArrowFunctions.js
@@ -0,0 +1,48 @@
+console.log("Arrow Functions");
+
+function mansi() {
+    console.log("Hello Mansi!");
+}
+
+mansi();
+
+const mansi2 = function() {
+    console.log("Hello Mansi 2!");
+}
+
+mansi2();
+
+// Valid syntax of an arrow function
+const mansi3 =() => {
+    console.log("Hello Mansi 3!");
+}
+
+mansi3();
+
+// Function returning something
+const greet = function () {
+    return "Good Morning";
+}
+console.log(greet());
+
+// arrow functions allow one liners
+// one line will automatically return, you dont need braces or "return"
+const greet2 = () => "Good evening";
+
+console.log(greet2());
+
+// to return object
+// you need parenthesis then
+const greet3 = () =>({name:"mansi"});
+
+console.log(greet3());
+
+
+// With one parameter you dont need paranthesis
+const greet4 = name => "Good evening " + name;
+
+console.log(greet4("Mansi"));
+
+const greet5 = (name,ending) => "Good evening " + name+" "+ending;
+
+console.log(greet5("Mansi","Jain"));
